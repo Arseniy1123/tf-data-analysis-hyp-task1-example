cee9698a0f349bb9b1e4f82680239a660ad2e94a
Arseniy Tushin <56977194+Arseniy1123@users.noreply.github.com>
2023-04-07 11:55:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import math
+from scipy.stats import norm
 
 chat_id = 975846018 # Ваш chat ID, не меняйте название переменной
 
@@ -12,11 +13,13 @@ def solution(x_success: int,
     p1 = x_success / x_cnt
     p2 = y_success / y_cnt
     
-    s = math.sqrt((p1 * (1 - p1)) / x_cnt + (p2 * (1 - p2)) / y_cnt)
+    p_pool = (x_success + y_success) / (x_cnt + y_cnt)
+    se = math.sqrt(p_pool * (1 - p_pool) * (1 / x_cnt + 1 / y_cnt))
     
-    z = (p2 - p1) / s
+    z_score = (p2 - p1) / se
+    p_value = 2 * (1 - norm.cdf(abs(z_score)))
     
     alpha = 0.06
-    z_crit = 1.88
+    reject_null = p_value < alpha
     
-    return abs(z) > z_crit
+    return reject_null
