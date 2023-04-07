a50582932d5ea498ea1514c1170a9a1508a4f48c
Arseniy Tushin <56977194+Arseniy1123@users.noreply.github.com>
2023-04-07 11:11:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import numpy as np
+import math
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 975846018 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+  
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    
+    SE = math.sqrt((p1 * (1 - p1)) / x_cnt + (p2 * (1 - p2)) / y_cnt)
+    
+    z = (p2 - p1) / SE
+    
+    alpha = 0.06
+    z_crit = 1.88
+    
+    return abs(z) > z_crit
